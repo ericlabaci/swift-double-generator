15657c0ea3947b907d6e80f9d60765a25f46cc5b
Raphael Carletti <raphael.carletti@farfetch.com>
2021-12-08 19:15:18 -0300

    Add make commands and remove generate-fixture not started yet

--- Makefile
+++ Makefile
@@ -1,3 +1,23 @@
+prefix ?= /usr/local
+bindir = $(prefix)/bin
+libdir = $(prefix)/lib
+
+build:
+	swift build -c release --disable-sandbox
+
+install: build
+	mv .build/release/DoubleGenerator .build/release/double-generator
+	install -d "$(bindir)" "$(libdir)"
+	install ".build/release/double-generator" "$(bindir)"
+
+uninstall:
+	rm -rf "$(bindir)/DoubleGenerator"
+
+clean:
+	rm -rf .build
+
+.PHONY: build install uninstall clean
+
 release:
 	rm -rf ./GeneratorDouble
 	swift build --configuration release
--- Sources/DoubleGenerator/main.swift
+++ Sources/DoubleGenerator/main.swift
@@ -6,7 +6,7 @@ import Stencil
 struct Double: ParsableCommand {
     static let configuration = CommandConfiguration(
         abstract: "A Swift command-line tool to auto generate doubles",
-        subcommands: [GenerateDouble.self, GenerateFixture.self])
+        subcommands: [GenerateDouble.self])
 
     init() { }
 }
@@ -43,37 +43,5 @@ struct GenerateDouble: ParsableCommand {
             print(error)
         }
 
-    }
-}
-
-struct GenerateFixture: ParsableCommand {
-
-    public static let configuration = CommandConfiguration(abstract: "Generate fixture for classes/structs")
-
-    @Argument(help: "The path of the file containing the protocols")
-    private var path: String
-
-    func run() throws {
-        print("Creating a fixture for file \"\(path)\"")
-
-        let file = File(path: path)!
-
-        let structure = try! Structure(file: file)
-
-        let data = structure.description.data(using: .utf8)
-        let codeStructure = try! JSONDecoder().decode(CodeStructure.self, from: data!)
-
-//        let protocols = codeStructure.substructures.filter { $0.kind == .protocol }
-//        let protocolStructures = protocols.map(ProtocolStructure.init)
-//        let protocolStencil = protocolStructures.map(ProtocolStencil.init)
-//
-//        let environment = Environment(loader: FileSystemLoader(paths: ["./Sources"]))
-//
-//        do {
-//            let content = try environment.renderTemplate(name: "template.stencil", context: ["protocols": protocolStencil])
-//            print(content)
-//        } catch {
-//            print(error)
-//        }
     }
 }
